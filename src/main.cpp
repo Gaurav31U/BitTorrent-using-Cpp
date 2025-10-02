7bc9358da3e6ed0e52df831f7a7efb5b8edba1fd
Gaurav31U <gaurav319288@gmail.com>
2025-10-03 02:37:47 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]) {
 
         // Uncomment this block to pass the first stage
         std::string encoded_value = argv[2];
-        json decoded_value = recursion_decode(encoded_value);
+        json decoded_value = recursion_decode(encoded_value,0);
         std::cout << decoded_value.dump() << std::endl;
     } else {
         std::cerr << "unknown command: " << command << std::endl;
