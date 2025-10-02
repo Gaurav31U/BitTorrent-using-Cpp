8f225f3d711130c6bb1114a34234fd3e9cfa3b48
Gaurav31U <gaurav319288@gmail.com>
2025-10-03 02:30:06 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,22 @@ json decode_bencoded_value(const std::string& encoded_value) {
     if (length == 0) {
         throw std::runtime_error("Empty encoded value");
     }
-    // Example: "i42e" -> 42
-    if (encoded_value[0] == 'i' && encoded_value[length - 1] == 'e') {
+    if (std::isdigit(encoded_value[0])) {
+        // Example: "5:hello" -> "hello"
+        size_t colon_index = encoded_value.find(':');
+        if (colon_index != std::string::npos) {
+            std::string number_string = encoded_value.substr(0, colon_index);
+            int64_t number = std::atoll(number_string.c_str());
+            std::string str = encoded_value.substr(colon_index + 1, number);
+            return json(str);
+        } else {
+            throw std::runtime_error("Invalid encoded value: " + encoded_value);
+        }
+    }else if (encoded_value[0] == 'i' && encoded_value[length - 1] == 'e') {
         std::string number_string = encoded_value.substr(1, length - 2);
         int64_t number = std::atoll(number_string.c_str());
         return json(number);
-    }else{
+    }else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
     }
 }
