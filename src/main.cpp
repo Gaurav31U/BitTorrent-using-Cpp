6e1218e9bc013862b4abec9bbcee324f963625a1
Gaurav31U <gaurav319288@gmail.com>
2025-10-03 02:27:15 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,10 @@
 using json = nlohmann::json;
 
 json decode_bencoded_value(const std::string& encoded_value) {
-    if (std::isdigit(encoded_value[0])) {
-        // Example: "5:hello" -> "hello"
-        size_t colon_index = encoded_value.find(':');
-        if (colon_index != std::string::npos) {
-            std::string number_string = encoded_value.substr(0, colon_index);
-            int64_t number = std::atoll(number_string.c_str());
-            std::string str = encoded_value.substr(colon_index + 1, number);
-            return json(str);
-        } else {
-            throw std::runtime_error("Invalid encoded value: " + encoded_value);
-        }
-    } else {
-        throw std::runtime_error("Unhandled encoded value: " + encoded_value);
-    }
+    int length = encoded_value.length();
+    std::string prefix = encoded_value.substr(0,length-2);
+    int64_t number = std::atoll(prefix.c_str());
+    return json(number);
 }
 
 int main(int argc, char* argv[]) {
